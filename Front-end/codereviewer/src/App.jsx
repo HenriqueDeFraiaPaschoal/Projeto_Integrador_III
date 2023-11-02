c00f60da28bb06dee8a0e21c9d5b7252519397bc
Victormoroo <victorhugo0609@outlook.com>
2023-11-02 14:07:06 -0300

    feat: instalada lib primereact para reating

--- Front-end/codereviewer/src/App.jsx
+++ Front-end/codereviewer/src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./App.css"
 import { AccessPage, Login, Register, Home } from "./routes"
+import { PrimeReactProvider } from "primereact/api"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AccessPage />}>
-          <Route index element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-        <Route path="/dashboard" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AccessPage />}>
+            <Route index element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+          <Route path="/dashboard" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </PrimeReactProvider>
   )
 }
 
