843ffea8c41e323c2840a16c0117ac60e1d4d9c4
Victormoroo <victorhugo0609@outlook.com>
2023-11-02 14:05:57 -0300

    feat: alteracoes na pagina home

--- Front-end/codereviewer/src/routes/home/index.jsx
+++ Front-end/codereviewer/src/routes/home/index.jsx
@@ -1,6 +1,6 @@
 import { PostBox, UsersBar } from "components"
 import "./style.css"
-import { MenuHamburguer } from "components"
+import { MenuHamburguer, CommentBox } from "components"
 
 import { userPost } from "mock" // remover esta linha quando integrar ao backend
 
@@ -9,6 +9,7 @@ export const Home = () => {
 
   return (
     <main className="home-container">
+      <button className="home-logout">SAIR</button>
       <div className="home-menu-container">
         <MenuHamburguer />
       </div>
@@ -16,7 +17,10 @@ export const Home = () => {
       <div className="home-content">
         <UsersBar user={userPost.user} />
         <PostBox body={body} title={ask} />
+        <p className="show-more" onClick={() => console.log('cliquei aqui...')}>...</p>
       </div>
+
+      <CommentBox />
     </main>
   )
 }
