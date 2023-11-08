2e8832ba9985fff05b3b9db08db0ec46163cba47
Victormoroo <victorhugo0609@outlook.com>
2023-11-08 19:46:48 -0300

    feat: atualizado componente home

--- Front-end/codereviewer/src/routes/home/index.jsx
+++ Front-end/codereviewer/src/routes/home/index.jsx
@@ -1,37 +1,34 @@
 import React from "react"
-import { PostBox, UsersBar } from "components"
-import "./style.css"
-import { MenuHamburguer, CommentBox } from "components"
-
-import { userPost } from "mock" // remover esta linha quando integrar ao backend
+import { UserPost } from "components"
+import { MenuHamburguer } from "components"
 import { useNavigate } from "react-router-dom"
-import { UserInfo } from "components/UserInfo"
+
+import { userPosts } from "mock" // remover esta linha quando integrar ao backend
+import "./style.css"
 
 export const Home = () => {
-
-  const navigate = useNavigate();  
-
-  const [showComments, setShowComments] = React.useState(false)
+  const navigate = useNavigate()
 
   return (
     <main className="home-container">
-      <button className="home-logout" onClick={() => navigate("/")}>SAIR</button>
+      <button className="home-logout" onClick={() => navigate("/")}>
+        SAIR
+      </button>
       <div className="home-menu-container">
         <MenuHamburguer />
       </div>
 
       <div className="home-content">
-        <UsersBar user={userPost[0].user} />
-        <PostBox body={userPost[0].question.body} title={userPost[0].question.ask} />
-        <p
-          className="show-more"
-          onClick={() => setShowComments((state) => !state)}
-        >
-          ...
-        </p>
-        <div className={`comment-box ${showComments ? "active" : ""}`}>
-          <CommentBox />
-        </div>
+        {userPosts &&
+          userPosts.map((data, index) => (
+            <UserPost
+              key={index}
+              user={data.user}
+              ask={data.question.ask}
+              answer={data.question.body}
+              comments={data.comments}
+            />
+          ))}
       </div>
     </main>
   )
