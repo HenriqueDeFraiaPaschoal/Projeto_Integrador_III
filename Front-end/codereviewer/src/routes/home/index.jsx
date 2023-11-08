b1bf24859fc98301f731f85395fb3e00b31c8934
Victormoroo <victorhugo0609@outlook.com>
2023-11-08 19:15:38 -0300

    feat: att na pagina home

--- Front-end/codereviewer/src/routes/home/index.jsx
+++ Front-end/codereviewer/src/routes/home/index.jsx
@@ -1,26 +1,38 @@
+import React from "react"
 import { PostBox, UsersBar } from "components"
 import "./style.css"
 import { MenuHamburguer, CommentBox } from "components"
 
 import { userPost } from "mock" // remover esta linha quando integrar ao backend
+import { useNavigate } from "react-router-dom"
+import { UserInfo } from "components/UserInfo"
 
 export const Home = () => {
-  const { ask, body } = userPost.question
+
+  const navigate = useNavigate();  
+
+  const [showComments, setShowComments] = React.useState(false)
 
   return (
     <main className="home-container">
-      <button className="home-logout">SAIR</button>
+      <button className="home-logout" onClick={() => navigate("/")}>SAIR</button>
       <div className="home-menu-container">
         <MenuHamburguer />
       </div>
 
       <div className="home-content">
-        <UsersBar user={userPost.user} />
-        <PostBox body={body} title={ask} />
-        <p className="show-more" onClick={() => console.log('cliquei aqui...')}>...</p>
+        <UsersBar user={userPost[0].user} />
+        <PostBox body={userPost[0].question.body} title={userPost[0].question.ask} />
+        <p
+          className="show-more"
+          onClick={() => setShowComments((state) => !state)}
+        >
+          ...
+        </p>
+        <div className={`comment-box ${showComments ? "active" : ""}`}>
+          <CommentBox />
+        </div>
       </div>
-
-      <CommentBox />
     </main>
   )
 }
