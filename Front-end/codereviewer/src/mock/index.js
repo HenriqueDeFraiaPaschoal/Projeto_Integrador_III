ccdf8445e1a30eaeffe544680e74c0beed1da726
Victormoroo <victorhugo0609@outlook.com>
2023-11-08 19:47:13 -0300

    feat: add novas informacoes

--- Front-end/codereviewer/src/mock/index.js
+++ Front-end/codereviewer/src/mock/index.js
@@ -1,4 +1,4 @@
-export const userPost = [
+export const userPosts = [
   {
     user: "Samuel",
     question: {
@@ -6,11 +6,10 @@ export const userPost = [
       body: "Olá pessoal! Estou com dificuldades em criar um loop em Python. Preciso percorrer uma lista de números e imprimir cada número. Alguém pode me ajudar?",
     },
     comments: [
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 1... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 2... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
     ],
   },
-
   {
     user: "Victor",
     question: {
@@ -18,8 +17,34 @@ export const userPost = [
       body: "Olá pessoal! Estou com dificuldades em criar um loop em Python. Preciso percorrer uma lista de números e imprimir cada número. Alguém pode me ajudar?",
     },
     comments: [
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 3... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 4... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 5... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+    ],
+  },
+  {
+    user: "Henrique",
+    question: {
+      ask: "Ajuda com loop em Python",
+      body: "Olá pessoal! Estou com dificuldades em criar um loop em Python. Preciso percorrer uma lista de números e imprimir cada número. Alguém pode me ajudar?",
+    },
+    comments: [
+      "Comentário 3... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 4... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 5... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+    ],
+  },
+  {
+    user: "Denis",
+    question: {
+      ask: "Ajuda com loop em Python",
+      body: "Olá pessoal! Estou com dificuldades em criar um loop em Python. Preciso percorrer uma lista de números e imprimir cada número. Alguém pode me ajudar?",
+    },
+    comments: [
+      "Comentário 3... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 4... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 5... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
+      "Comentário 6... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabomodi aspernatur ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad eos ex asperiores corporis quidem ullam nesciunt quasi, quae molestias minus vel veniam expedita consequatur assumenda explicabo modi aspernatur ea. ",
     ],
   },
 ]
