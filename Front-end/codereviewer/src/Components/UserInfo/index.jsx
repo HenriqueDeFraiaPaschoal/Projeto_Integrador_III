a3f6e7506c48109887d2ef13396cc55df39b12eb
Victormoroo <victorhugo0609@outlook.com>
2023-11-08 19:20:21 -0300

    feat: criado componente e style informacoes do usuario

---
+++ Front-end/codereviewer/src/Components/UserInfo/index.jsx
@@ -0,0 +1,25 @@
+import { IconProfile } from "assets/icons/IconProfile"
+import { Medalha01 } from "assets/icons/medalhas/Medalha01"
+import { Medalha02 } from "assets/icons/medalhas/Medalha02"
+import { Medalha04 } from "assets/icons/medalhas/Medalha04"
+import "./style.css"
+
+export const UserInfo = () => {
+  return (
+    <>
+      <div className="user-container-profile">
+        <p>Usuário123</p>
+        <IconProfile />
+      </div>
+      <div className="medal-container">
+        <p>Medalhas</p>
+        <div className="medal-content">
+          <Medalha01 />
+          <Medalha02 />
+          {/* <Medalha03 /> */}
+          <Medalha04 />
+        </div>
+      </div>
+    </>
+  )
+}
