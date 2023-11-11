7b8a42327300e5312040f4a534d9d05a943b1fda
Victormoroo <victorhugo0609@outlook.com>
2023-11-11 13:42:08 -0300

    feat: att UserPost

--- Front-end/codereviewer/src/Components/UserPost/index.jsx
+++ Front-end/codereviewer/src/Components/UserPost/index.jsx
@@ -5,24 +5,27 @@ import "./style.css"
 export const UserPost = ({ user, ask, answer, comments }) => {
   const [showComments, setShowComments] = React.useState(false)
 
+  const toggleComments = () => {
+    setShowComments((state) => !state)
+  }
+
   return (
     <>
       <UsersBar user={user} />
       <PostBox body={answer} title={ask} />
-      <p
-        className="show-more"
-        onClick={() => setShowComments((state) => !state)}
-      >
+      <p className="show-more" onClick={toggleComments}>
         ...
       </p>
-
-      {showComments && comments && (
-        <div className="box">
-          <div className="comment-container">
-            {comments.map((comment, index) => (
+      <div className={`box ${showComments ? "open" : ""}`}>
+        <div className="comment-container">
+          {comments &&
+            comments.map((comment, index) => (
               <CommentBox key={index} comments={comment} />
             ))}
-          </div>
+        </div>
+      </div>
+      {showComments && (
+        <div className="btn-container">
           <button className="comment-button">Comentar</button>
         </div>
       )}
