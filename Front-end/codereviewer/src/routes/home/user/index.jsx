4ec3db591ea99581dca57a7a757a6742c14fd5f2
Victormoroo <victorhugo0609@outlook.com>
2023-11-24 18:40:45 -0300

    feat: modificado pagina user

--- Front-end/codereviewer/src/routes/home/user/index.jsx
+++ Front-end/codereviewer/src/routes/home/user/index.jsx
@@ -1,11 +1,28 @@
 import { InputComment } from "components/InputComment"
 import { UserInfo } from "components/UserInfo"
+import { UserPost } from "components"
+import { userPosts } from "mock"
+import "./style.css"
 
 export const User = () => {
-  return(
-    <>
-      <UserInfo />
-      <InputComment />
-    </>
+  return (
+    <div className="user-info-container">
+      <div>
+        <UserInfo />
+        {/* <InputComment /> */}
+      </div>
+      <div className="home-content">
+        {userPosts &&
+          userPosts.map((data, index) => (
+            <UserPost
+              key={index}
+              user={data.user}
+              ask={data.question.ask}
+              answer={data.question.body}
+              comments={data.comments}
+            />
+          ))}
+      </div>
+    </div>
   )
 }
