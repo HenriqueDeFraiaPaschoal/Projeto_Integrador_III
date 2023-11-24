b8a5fad2dc0f53a1b6ecead40925979d7eae67c1
Victormoroo <victorhugo0609@outlook.com>
2023-11-24 19:08:16 -0300

    feat: adicionado cors, e resolvido problema com cadastro e login

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -1,9 +1,11 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
