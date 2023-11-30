307ed09c4b7bc648f94c79233839c19f1fc5b0db
Victormoroo <victorhugo0609@outlook.com>
2023-11-29 22:07:16 -0300

    feat: criada logica para post no modal

--- Front-end/codereviewer/src/components/Modal/index.jsx
+++ Front-end/codereviewer/src/components/Modal/index.jsx
@@ -1,18 +1,37 @@
 import { IoMdCloseCircleOutline } from "react-icons/io";
 import React from "react";
 import "./style.css";
+import { NewPost } from "services/users";
 
 export const Modal = ({ onClose, ...props }) => {
-  const handlePublish = () => {
+  const [post, setPost] = React.useState();
+  const [userID, setUserID] = React.useState("");
+
+  const handlePublish = React.useCallback(async () => {
     // Exibe a caixa de diálogo de confirmação
-    const shouldPublish = window.confirm("Deseja realmente publicar seu código?");
+    const shouldPublish = window.confirm(
+      "Deseja realmente publicar seu código?"
+    );
 
     // Se o usuário escolher "OK", continua com a ação de publicar
+    const data = { content: post, postDate: new Date(), userId: userID };
+
     if (shouldPublish) {
       console.log("Publicando o código...");
       // lógica da publicação
+      try {
+        const response = await NewPost(data);
+        const info = await response.data;
+
+      } catch (e) {
+        // Adicionar tratamento de erro...
+      }
     }
-  };
+  }, [post, userID]);
+  React.useEffect(() => {
+    setUserID(localStorage.getItem("USER_ID" || ""));
+  }, []);
+
 
   return (
     <div className="modal-overlay">
@@ -30,6 +49,8 @@ export const Modal = ({ onClose, ...props }) => {
             cols=""
             rows=""
             placeholder="Escreva..."
+            value={post}
+            onChange={(e) => setPost(e.target.value)}
           ></textarea>
           <div className="btn-post-container">
             <button className="btn-post" onClick={handlePublish}>
