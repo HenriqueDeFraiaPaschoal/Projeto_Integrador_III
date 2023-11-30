97537d31580b2ef22f904a8a28803357996f8188
Victormoroo <victorhugo0609@outlook.com>
2023-11-29 22:08:56 -0300

    feat: criada novas rotas de service

--- Front-end/codereviewer/src/services/users.js
+++ Front-end/codereviewer/src/services/users.js
@@ -1,8 +1,18 @@
 import axios from "axios";
-import { URL_BASE } from "constants"
+import { URL_BASE } from "constants";
 
-
+// para login precisa de user e password
 export const Signin = async (data) => {
-    const response = await axios.post(`${URL_BASE}/users`, data)
-    return response
-} 
+  const response = await axios.post(`${URL_BASE}/users`, data);
+  return response;
+};
+
+export const NewPost = async (post) => {
+  const response = await axios.post(`${URL_BASE}/post`, post);
+  return response.data;
+};
+
+export const GetAllPosts = async () => {
+  const response = await axios.get(`${URL_BASE}/posts`);
+  return response.data;
+};
