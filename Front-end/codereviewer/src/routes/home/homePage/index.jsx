0ec4e29641083eb527a9a17ecd4c0a88781173f4
Victormoroo <victorhugo0609@outlook.com>
2023-11-29 22:09:40 -0300

    feat: pagina home com os posts do banco

--- Front-end/codereviewer/src/routes/home/homePage/index.jsx
+++ Front-end/codereviewer/src/routes/home/homePage/index.jsx
@@ -1,20 +1,35 @@
-import './style.css'
-import { UserPost } from "components"
-import { userPosts } from "mock" // remover esta linha quando integrar ao backend
+import React from "react";
+import "./style.css";
+import { UserPost } from "components";
+import { GetAllPosts } from "services/users";
+//import { userPosts } from "mock" // remover esta linha quando integrar ao backend
 
 export const HomePage = () => {
+  const [userPosts, setUserPosts] = React.useState([]);
+
+  const getPosts = React.useCallback(async () => {
+    try {
+      const response = await GetAllPosts();
+      setUserPosts(response);
+      console.log(response);
+    } catch (e) {}
+  }, []);
+
+  React.useEffect(() => {
+    getPosts();
+  }, [getPosts]);
   return (
     <div className="home-content">
       {userPosts &&
         userPosts.map((data, index) => (
           <UserPost
             key={index}
-            user={data.user}
-            ask={data.question.ask}
-            answer={data.question.body}
-            comments={data.comments}
+            user={data.id}
+            ask={data.content}
+            // answer={data.question.ask}
+            //comments={data.comments}
           />
         ))}
     </div>
-  )
-}
+  );
+};
